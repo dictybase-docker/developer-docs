4391de1ca2f7238b6e3f4b199a0a1f7d970ba448
wildlifehexagon <crossthebreeze@gmail.com>
2020-05-26 15:48:33 -0500

    feat: add logic for generating pages based on md files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,42 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <Link to={node.fields.slug}>{node.frontmatter.title}</Link>
+          </div>
+        ))}
+      </div>
+    </Layout>
+  )
+}
 
+const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___title], order: ASC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+          }
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`
+
+export { query }
 export default IndexPage
