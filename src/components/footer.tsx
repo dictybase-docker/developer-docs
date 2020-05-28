91db6f13567c8d4e6e9dd911dd8324e0bef7b973
wildlifehexagon <crossthebreeze@gmail.com>
2020-05-28 10:49:57 -0500

    refactor: convert footer to mui

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,26 +1,30 @@
 import React from "react"
+import { makeStyles } from "@material-ui/core/styles"
 
-const Footer = () => (
-  <footer
-    style={{
-      background: `rgb(0, 64, 128)`,
-      textAlign: "center",
-    }}>
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}>
-      <h5
-        style={{
-          margin: 0,
-          color: "#fff",
-        }}>
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    background: `rgb(0, 64, 128)`,
+    textAlign: "center",
+    margin: `0 auto`,
+    padding: `1.45rem 1.0875rem`,
+    width: "100%",
+  },
+  copyright: {
+    margin: 0,
+    color: "#fff",
+  },
+}))
+
+const Footer = () => {
+  const classes = useStyles()
+
+  return (
+    <footer className={classes.footer}>
+      <h5 className={classes.copyright}>
         © {new Date().getFullYear()} dictyBase
       </h5>
-    </div>
-  </footer>
-)
+    </footer>
+  )
+}
 
 export default Footer
