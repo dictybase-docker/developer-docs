4234d4c31c42f6d088a52cac8e4bcdf7738ed729
Siddhartha Basu <biosidd@gmail.com>
2020-06-02 12:57:40 -0500

    chore:add github css cdn link

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -47,7 +47,9 @@ function SEO({ description, lang, meta, title }) {
           content: `summary`,
         },
       ].concat(meta)}
-    />
+    >
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/4.0.0/github-markdown.min.css" integrity="sha256-gzohnzxILb7OZZch6c8mySnK1r0yFviwmBR+1E5O0RM=" crossorigin="anonymous" />
+    </Helmet>
   )
 }
 
