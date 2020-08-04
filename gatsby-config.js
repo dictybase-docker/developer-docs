c3f03ab783025a03c6bb33b72afc1be1ebc6917a
Siddhartha Basu <biosidd@gmail.com>
2020-08-04 16:51:54 -0500

    feat:add table of contents plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,6 +54,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-material-ui`,
+    'gatsby-remark-table-of-contents',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
