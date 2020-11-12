10dfc3bfb332517d9d265b76d2af133c0225f809
wildlifehexagon <hartline.eric@gmail.com>
2020-11-12 02:56:15 +0000

    refactor: add smooth-doc template, reorganize files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,72 +1,15 @@
 module.exports = {
-  siteMetadata: {
-    title: `dictyBase Developer Docs`,
-    description: `All documentation related to developing dictyBase software.`,
-    menuLinks: [
-      {
-        name: "Deployment",
-        link: "/deployment",
-      },
-    ],
-  },
-  pathPrefix: `/developer-docs`,
   plugins: [
-    `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'smooth-doc',
       options: {
-        name: `src/pages`,
-        path: `${__dirname}/src/pages`,
+        name: 'dictyBase Developer Docs',
+        description: 'All documentation related to developing dictyBase software.',
+        siteUrl: 'https://dictybase-docker.github.io/developer-docs/',
+        sections: ['Deployment'],
+        navItems: [{ title: 'Deployment', url: '/docs/deployment/' }],
+        githubRepositoryURL: 'https://github.com/dictybase-docker/developer-docs',
       },
     },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          `gatsby-remark-autolink-headers`,
-          {
-            resolve: `gatsby-remark-table-of-contents`,
-            options: {
-              fromHeading: 1,
-              toHeading: 6,
-            },
-          },
-          {
-            resolve: "gatsby-remark-code-buttons",
-            options: {
-              tooltipText: `Copy`,
-            },
-          },
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 800,
-              wrapperStyle: "margin-left: 25px;",
-            },
-          },
-          {
-            resolve: `gatsby-remark-prismjs`,
-          },
-        ],
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `dictybase-dev-docs`,
-        short_name: `dev-docs`,
-        start_url: `/`,
-        background_color: `rgb(0, 64, 128)`,
-        theme_color: `rgb(0, 64, 128)`,
-        display: `minimal-ui`,
-        // icon: `src/images/dicty-logo.png`, // This path is relative to the root of the site.
-      },
-    },
-    `gatsby-plugin-material-ui`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
