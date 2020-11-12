5c865e654c0c84bf3060f0b114eddd3e23f81d54
wildlifehexagon <crossthebreeze@gmail.com>
2020-11-12 10:51:22 -0600

    refactor: add base directory

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,17 @@
 module.exports = {
   plugins: [
     {
-      resolve: 'smooth-doc',
+      resolve: "smooth-doc",
       options: {
-        name: 'dictyBase Developer Docs',
-        description: 'All documentation related to developing dictyBase software.',
-        siteUrl: 'https://dictybase-docker.github.io/developer-docs/',
-        sections: ['Deployment'],
-        navItems: [{ title: 'Deployment', url: '/docs/deployment/' }],
-        githubRepositoryURL: 'https://github.com/dictybase-docker/developer-docs',
+        name: "dictyBase Developer Docs",
+        description:
+          "All documentation related to developing dictyBase software.",
+        siteUrl: "https://dictybase-docker.github.io/developer-docs/",
+        sections: ["Deployment"],
+        navItems: [{ title: "Deployment", url: "/docs/deployment/" }],
+        githubRepositoryURL:
+          "https://github.com/dictybase-docker/developer-docs",
+        baseDirectory: __dirname,
       },
     },
   ],
